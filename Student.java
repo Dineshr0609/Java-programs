a9a6dd4ffd06318a1dec7b19a02bf144accaceea
Dinesh0609 <ravipatidinesh20@gmail.com>
2024-01-11 10:06:01 -0600

    Student program displays name

---
+++ Student.java
@@ -0,0 +1,14 @@
+
+public class Student {
+	String name = "Dinesh";
+    
+    String getName() {
+    	return name;
+    }
+    
+    	public static void main(String[] args) {
+    	Student student = new Student();
+		System.out.println(student.getName());
+	}
+	
+}
